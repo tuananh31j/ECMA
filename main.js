d8f392270dcbe65d4f2b6fad6aaf0faece0c2331
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-09-18 18:03:30 +0700

    config project

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  
+<div>Haha</div>
+`
+
+setupCounter(document.querySelector('#counter'))
